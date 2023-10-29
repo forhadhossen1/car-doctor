6d0c368e6d3850ec4309345b7272c7f0924ad199
forhadhossen1 <mr.forhad1030@gamil.com>
2023-10-30 05:15:22 +0600

    booking service confirmed section

---
+++ src/Pages/Bookings/BookingTable.jsx
@@ -0,0 +1,40 @@
+
+const BookingTable = ({ booking, handleDelete, handleConfirm }) => {
+    const { date, img, service, price, _id, status } = booking || {}
+
+   
+
+    return (
+
+        <tr>
+            <th>
+                <button onClick={()=>handleDelete(_id)} className="btn btn-sm btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </th>
+            <td>
+                <div className="avatar">
+                    <div className="rounded w-12 h-12">
+                        <img src={img} alt="client img" />
+                    </div>
+                </div>
+            </td>
+            <td>
+                {service}
+            </td>
+            <td>{date}</td>
+            <td>{price}</td>
+            <th>
+
+                {
+                    status === 'confirm'? <span className="font-bold text-orange-700">Confirmed</span> :
+                    <button onClick={()=>handleConfirm(_id)} className="btn btn-ghost btn-xs">Please Confirm</button>
+                }
+
+            </th>
+        </tr>
+
+    );
+};
+
+export default BookingTable;
